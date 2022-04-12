b4ebbfb6e96bfdada5622e206e97df3e4c89e85e
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-04-12 23:39:02 +0300

    Add recycler view and its components to visualize strings from strings.xml resource file

--- app/src/main/java/com/dst/affirmations/MainActivity.kt
+++ app/src/main/java/com/dst/affirmations/MainActivity.kt
@@ -2,10 +2,17 @@ package com.dst.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.dst.affirmations.adapter.ItemAdapter
+import com.dst.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
     }
 }
