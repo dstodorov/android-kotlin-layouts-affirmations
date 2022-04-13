b156329a30b5ecd1a4625246c60683e9efce5e25
Dimitar Todorov <d.todorov.vn@gmail.com>
2022-04-13 22:20:17 +0300

    Add image for every Affirmation. Make more fancy design

---
+++ app/src/test/java/com/dst/affirmations/AffirmationsAdapterTests.kt
@@ -0,0 +1,22 @@
+package com.dst.affirmations
+
+import android.content.Context
+import com.dst.affirmations.adapter.ItemAdapter
+import com.dst.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class AffirmationsAdapterTests {
+    private val context = mock(Context::class.java)
+
+    @Test
+    fun adapter_test() {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+}
